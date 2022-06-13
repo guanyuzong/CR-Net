972df0050c403180e3b6a78a1d0b0aa78bac3c89
guanyuzong <78291941+guanyuzong@users.noreply.github.com>
2022-06-13 16:34:17 +0800

    Add files via upload

---
+++ CRNet_test.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn.functional as F
+import sys
+sys.path.append('./models')
+import numpy as np
+import os, argparse
+import cv2
+from models.CRNet_model import BBSNet
+from data import test_dataset
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--testsize', type=int, default=352, help='testing size')
+parser.add_argument('--gpu_id', type=str, default='0', help='select gpu id')
+parser.add_argument('--test_path',type=str,default='./CR_dataset/RGBD_for_test/',help='test dataset path')
+opt = parser.parse_args()
+
+dataset_path = opt.test_path
+
+#set device for test
+if opt.gpu_id=='0':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    print('USE GPU 0')
+elif opt.gpu_id=='1':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    print('USE GPU 1')
+elif opt.gpu_id=='2':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    print('USE GPU 2')
+#load the model
+model = BBSNet()
+#Large epoch size may not generalize well. You can choose a good model to load according to the log file and pth files saved in ('./BBSNet_cpts/') when training.
+model.load_state_dict(torch.load('./model_pths/CRNet_epoch_best.pth'))
+model.cuda()
+model.eval()
+
+#test
+test_datasets = ['NJU2K','NLPR','STERE', 'DES', 'SSD','LFSD','SIP']
+for dataset in test_datasets:
+    save_path = './test_maps/CR-Net/' + dataset + '/'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    image_root = dataset_path + dataset + '/RGB/'
+    gt_root = dataset_path + dataset + '/GT/'
+    depth_root=dataset_path +dataset +'/depth/'
+    test_loader = test_dataset(image_root, gt_root,depth_root, opt.testsize)
+    for i in range(test_loader.size):
+        image, gt,depth, name, image_for_post = test_loader.load_data()
+        gt = np.asarray(gt, np.float32)
+        gt /= (gt.max() + 1e-8)
+        image = image.cuda()
+        depth = depth.cuda()
+        _,_,_,_,_,res,_,_,_,_,_,_ = model(image,depth)
+        res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
+        res = res.sigmoid().data.cpu().numpy().squeeze()
+        res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+        print('save img to: ',save_path+name)
+        cv2.imwrite(save_path+name,res*255)
+    print('Test Done!')
---
+++ CRNet_train.py
@@ -0,0 +1,171 @@
+import os
+import torch
+import torch.nn.functional as F
+import sys
+sys.path.append('./models')
+import numpy as np
+from datetime import datetime
+from torchvision.utils import make_grid
+from models.CRNet_model import BBSNet
+from data import get_loader,test_dataset
+from utils import clip_gradient, adjust_lr
+from tensorboardX import SummaryWriter
+import logging
+import torch.backends.cudnn as cudnn
+from options import opt
+import pytorch_iou
+
+#set the device for training
+if opt.gpu_id=='0':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    print('USE GPU 0')
+elif opt.gpu_id=='1':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    print('USE GPU 1')
+elif opt.gpu_id=='2':
+    os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+    print('USE GPU 2')
+elif opt.gpu_id=='3':
+    os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+    print('USE GPU 3')
+cudnn.benchmark = True
+
+#build the model
+model = BBSNet()
+if(opt.load is not None):
+    model.load_state_dict(torch.load(opt.load))
+    print('load model from ',opt.load)
+    
+model.cuda()
+params = model.parameters()
+optimizer = torch.optim.Adam(params, opt.lr)
+
+#set the path
+image_root = opt.rgb_root
+gt_root = opt.gt_root
+depth_root=opt.depth_root
+test_image_root=opt.test_rgb_root
+test_gt_root=opt.test_gt_root
+test_depth_root=opt.test_depth_root
+save_path=opt.save_path
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+#load data
+print('load data...')
+train_loader = get_loader(image_root, gt_root,depth_root, batchsize=opt.batchsize, trainsize=opt.trainsize)
+test_loader = test_dataset(test_image_root, test_gt_root,test_depth_root, opt.trainsize)
+total_step = len(train_loader)
+
+logging.basicConfig(filename=save_path+'log.log',format='[%(asctime)s-%(filename)s-%(levelname)s:%(message)s]', level = logging.INFO,filemode='a',datefmt='%Y-%m-%d %I:%M:%S %p')
+logging.info("BBSNet-Train")
+logging.info("Config")
+logging.info('epoch:{};lr:{};batchsize:{};trainsize:{};clip:{};decay_rate:{};load:{};save_path:{};decay_epoch:{}'.format(opt.epoch,opt.lr,opt.batchsize,opt.trainsize,opt.clip,opt.decay_rate,opt.load,save_path,opt.decay_epoch))
+
+#set loss function
+CE = torch.nn.BCEWithLogitsLoss()
+IOU = pytorch_iou.IOU(size_average=True)
+step=0
+writer = SummaryWriter(save_path+'summary')
+best_mae=1
+best_epoch=0
+
+#train function
+def train(train_loader, model, optimizer, epoch,save_path):
+    global step
+    model.train()
+    loss_all=0
+    epoch_step=0
+    try:
+        for i, (images, gts, depths) in enumerate(train_loader, start=1):
+            optimizer.zero_grad()
+            
+            images = images.cuda()
+            gts = gts.cuda()
+            depths=depths.cuda()
+
+            s0,s1,s2,s3,s4,s5,s0_sig,s1_sig,s2_sig,s3_sig,s4_sig,s5_sig = model(images,depths)
+            loss0 = CE(s0,gts)+IOU(s0_sig,gts)
+            loss1 = CE(s1, gts)+IOU(s1_sig,gts)
+            loss2 = CE(s2,gts)+IOU(s2_sig,gts)
+            loss3 = CE(s3, gts)+IOU(s3_sig,gts)
+            loss4 = CE(s4,gts)+IOU(s4_sig,gts)
+            loss5 = CE(s5,gts)+IOU(s5_sig,gts)
+            loss = 0.1*loss0 + 0.3*loss1 + 0.5*loss2+ 0.7*loss3 + 0.9*loss4 + 1*loss5
+            loss.backward()
+
+            clip_gradient(optimizer, opt.clip)
+            optimizer.step()
+            step+=1
+            epoch_step+=1
+            loss_all+=loss.data
+            if i % 100 == 0 or i == total_step or i==1:
+                print('{} Epoch [{:03d}/{:03d}], Step [{:04d}/{:04d}],  Loss0: {:.4f} Loss1: {:.4f} Loss2: {:0.4f} Loss3: {:0.4f} Loss4: {:0.4f} Loss5: {:0.4f} Total_Loss: {:0.4f}'.
+                    format(datetime.now(), epoch, opt.epoch, i, total_step, loss0.data, loss1.data, loss2.data,loss3.data,loss4.data,loss5.data,loss.data))
+                logging.info('#TRAIN#:Epoch [{:03d}/{:03d}], Step [{:04d}/{:04d}], Loss1: {:.4f} Loss2: {:0.4f}'.
+                    format( epoch, opt.epoch, i, total_step, loss1.data, loss2.data))
+                writer.add_scalar('Loss', loss.data, global_step=step)
+                grid_image = make_grid(images[0].clone().cpu().data, 1, normalize=True)
+                writer.add_image('RGB', grid_image, step)
+                grid_image = make_grid(gts[0].clone().cpu().data, 1, normalize=True)
+                writer.add_image('Ground_truth', grid_image, step)
+                res=s1[0].clone()
+                res = res.sigmoid().data.cpu().numpy().squeeze()
+                res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+                writer.add_image('s1', torch.tensor(res), step,dataformats='HW')
+                res=s2[0].clone()
+                res = res.sigmoid().data.cpu().numpy().squeeze()
+                res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+                writer.add_image('s2', torch.tensor(res), step,dataformats='HW')
+        
+        loss_all/=epoch_step
+        logging.info('#TRAIN#:Epoch [{:03d}/{:03d}], Loss_AVG: {:.4f}'.format( epoch, opt.epoch, loss_all))
+        writer.add_scalar('Loss-epoch', loss_all, global_step=epoch)
+        if (epoch) % 5 == 0:
+            torch.save(model.state_dict(), save_path+'BBSNet_epoch_{}.pth'.format(epoch))
+    except KeyboardInterrupt: 
+        print('Keyboard Interrupt: save model and exit.')
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        torch.save(model.state_dict(), save_path+'BBSNet_epoch_{}.pth'.format(epoch+1))
+        print('save checkpoints successfully!')
+        raise
+        
+#test function
+def test(test_loader,model,epoch,save_path):
+    global best_mae,best_epoch
+    model.eval()
+    with torch.no_grad():
+        mae_sum=0
+        for i in range(test_loader.size):
+            image, gt,depth, name,img_for_post = test_loader.load_data()
+            gt = np.asarray(gt, np.float32)
+            gt /= (gt.max() + 1e-8)
+            image = image.cuda()
+            depth = depth.cuda()
+            _,_,_,_,_,res,_,_,_,_,_,_ = model(image,depth)
+            res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
+            res = res.sigmoid().data.cpu().numpy().squeeze()
+            res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+            mae_sum+=np.sum(np.abs(res-gt))*1.0/(gt.shape[0]*gt.shape[1])
+        mae=mae_sum/test_loader.size
+        writer.add_scalar('MAE', torch.tensor(mae), global_step=epoch)
+        print('Epoch: {} MAE: {} ####  bestMAE: {} bestEpoch: {}'.format(epoch,mae,best_mae,best_epoch))
+        if epoch==1:
+            best_mae=mae
+        else:
+            if mae<best_mae:
+                best_mae=mae
+                best_epoch=epoch
+                torch.save(model.state_dict(), save_path+'BBSNet_epoch_best.pth')
+                print('best epoch:{}'.format(epoch))
+        logging.info('#TEST#:Epoch:{} MAE:{} bestEpoch:{} bestMAE:{}'.format(epoch,mae,best_epoch,best_mae))
+ 
+if __name__ == '__main__':
+    print("Start train...")
+    for epoch in range(1, opt.epoch):
+        cur_lr=adjust_lr(optimizer, opt.lr, epoch, opt.decay_rate, opt.decay_epoch)
+        writer.add_scalar('learning_rate', cur_lr, global_step=epoch)
+        train(train_loader, model, optimizer, epoch,save_path)
+        test(test_loader,model,epoch,save_path)
